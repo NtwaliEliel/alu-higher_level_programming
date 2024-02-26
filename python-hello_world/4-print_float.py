cc45bcfaf8821a3c8bc1e60f1faf4b3e8502b877
NtwaliEliel <you@example.com>
2024-02-26 09:49:52 -0800

    up12

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 number = 3.14159
-print(f"Float: {number:,.2f}\n")
+print(f"Float: {number:,.2f}")
 sys.stderr = open('err.txt', 'w')
 print("[Anything", file=sys.stderr)
