82f57d426d9fb44befc169d4281cfe8df1555c06
NtwaliEliel <you@example.com>
2024-03-07 02:57:09 -0800

    ui

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,7 +2,7 @@
 """This module Define square based on 3-square.py"""
 
 
-class square:
+class Square:
     """This Define Square"""
 
     def __init__(self, size=0):
@@ -10,17 +10,21 @@ class square:
 
         self.__size = size
 
-     @property
-     def size(self):
+    @property
+    def size(self):
          """This finds size by getter methods"""
          return self.__size
 
-     @size.setter
-     def size(self, value):
-         "This validate size by setter method"""
-         self.__size = value
+    @size.setter
+    def size(self, value):
+        "This validate size by setter method"""
+        self.__size = value
 
-         if type(value) != int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """We are going to create an area for Square"""
+        return self.__size ** 2
