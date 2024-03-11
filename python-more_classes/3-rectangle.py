a09b2685e1978b988c30999983a39017648add9b
NtwaliEliel <you@example.com>
2024-03-11 02:24:26 -0700

    up7

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+"""No module Imported"""
+
+
+class Rectangle:
+
+    """Defining Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialization of Data"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Accessing width (Getter method)"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Defining width by setter methods"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Accessing height by Getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Defining height by setter"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Defining Area of Rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Defining the Perimeter of Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """String Creation"""
+        s = ''
+        if self.__width > 0 and self.__height > 0:
+            for _ in range(self.__height):
+                for _ in range(self.__width):
+                    s += '#'
+                s += '\n'
+        return s[: -1]
