3d4787616b0c13a64989bd4ea7b22a14de72fc41
NtwaliEliel <you@example.com>
2024-03-13 00:16:36 -0700

    Update

---
+++ python-more_classes/9-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+
+"""No module imported"""
+
+class Rectangle:
+
+    """Defining Rectangle"""
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """Initialization"""
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Accessing width by Getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Defining width by setter method"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Accessing height by Getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Defining height by setter method"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Defining area of the Rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Defining perimeter of the Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    @classmethod
+    def square(cls, size=0):
+        """This Returns a new Rectangle instance with width == height == size"""
+        return cls(size, size)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Bigger Rectangle Returns based on area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+    def __str__(self):
+        """Creating String Representation of Rectangle"""
+        s = ''
+        if self.__width > 0 or self.__height > 0:
+            for _ in range(self.__height):
+                for _ in range(self.__width):
+                    s += str(self.print_symbol)
+                s += '\n'
+        return s[: -1]
+
+    def __repr__(self):
+        """Conical string Represantation object"""
+        return 'Rectangle({}, {})'.format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print a closure"""
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
