e60c31ed85307140b6bbdb60fd8c780607f7ed2a
NtwaliEliel <you@example.com>
2024-03-15 00:58:51 -0700

    Update

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""No module imported"""
+
+
+class MyList(list):
+
+    """Mylist Inherite from list"""
+
+    def print_sorted(self):
+        """Listing"""
+        print(sorted(self):
