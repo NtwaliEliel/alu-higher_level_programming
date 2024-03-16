a66ddf177a5e23f46a2f9ca35febf9d41df1e411
NtwaliEliel <you@example.com>
2024-03-16 00:17:12 -0700

    Update

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""json imported"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writing using json"""
+    with open(filename, mode='w') as f:
+        json.dump(my_obj, f, ensure.ascii=False)
