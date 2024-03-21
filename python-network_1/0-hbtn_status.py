4ee5e93bf3748e4350eea62681d4c2d20f9fcf03
NtwaliEliel <you@example.com>
2024-03-21 02:09:39 -0700

    Update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""Fetches https://alu-intranet.hbtn.io/status"""
 if __name__ == "__main__":
     from urllib import request
 
     with request.urlopen("https://alu-intranet.hbtn.io/status") as f:
         status = f.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(status)))
-        print("\t- content: {}".format(status))
-        print("\t- utf8 content: {}".format(status.decode("utf-8")))
+        print("Body response:\n"
+              "\t- type: {}\n"
+              "\t- content: {}\n"
+              "\t- utf8 content: {}".format(type(status), status,
+                                            status.decode("utf-8")))
